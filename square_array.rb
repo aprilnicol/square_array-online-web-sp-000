41f64ac5ea5f3d9c18d870cc8733ea1b104cfe87
April Adams <illusionlace@gmail.com>
2020-08-18 09:34:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-numbers = [1,2,3]
-square_array(numbers).each
-new_numbers = [1,4,9]
-square_array(new_numbers).each 
+arrays = []
+array.each do |numbers|
+  numbers== numbers**2
+  arrays << numbers 
+end 
 
 
 end
