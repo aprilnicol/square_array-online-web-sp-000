527837b7bab2c70d0d1c6b1e2499249fa5a1f6ec
April Adams <illusionlace@gmail.com>
2020-08-18 09:30:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
 numbers = [1,2,3]
-square_array(numbers)
+square_array(numbers).each
 new_numbers = [1,4,9]
-square_array(new_numbers)
+square_array(new_numbers).each 
 
 
 end
